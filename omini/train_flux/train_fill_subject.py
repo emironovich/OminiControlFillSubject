2e93848cae89de535f94d6ea80bc58b132efb66f
Elizaveta Mironovich <elizavetamironovich@gmail.com>
2026-02-06 17:23:31 +0100

    Put ImageMultiConditionDataset as base Dataset

--- omini/train_flux/train_fill_subject.py
+++ omini/train_flux/train_fill_subject.py
@@ -11,10 +11,47 @@ from .trainer import OminiModel, get_config, train
 from ..pipeline.flux_omini import Condition, generate
 
 
-class CustomDataset(Dataset):
+class ImageMultiConditionDataset(ImageConditionDataset):
     def __getitem__(self, idx):
-        # TODO: Implement the logic to load your custom dataset
-        raise NotImplementedError("Custom dataset loading not implemented")
+        image = self.base_dataset[idx]["jpg"]
+        image = image.resize(self.target_size).convert("RGB")
+        description = self.base_dataset[idx]["json"]["prompt"]
+
+        condition_size = self.condition_size
+        position_scale = self.position_scale
+
+        condition_imgs, position_deltas = [], []
+        for c_type in self.condition_type:
+            condition_img, position_delta = self.__get_condition__(image, c_type)
+            condition_imgs.append(condition_img.convert("RGB"))
+            position_deltas.append(position_delta)
+
+        # Randomly drop text or image (for training)
+        drop_text = random.random() < self.drop_text_prob
+        drop_image = random.random() < self.drop_image_prob
+
+        if drop_text:
+            description = ""
+        if drop_image:
+            condition_imgs = [
+                Image.new("RGB", condition_size)
+                for _ in range(len(self.condition_type))
+            ]
+
+        return_dict = {
+            "image": self.to_tensor(image),
+            "description": description,
+            **({"pil_image": [image, condition_img]} if self.return_pil_image else {}),
+        }
+
+        for i, c_type in enumerate(self.condition_type):
+            return_dict[f"condition_{i}"] = self.to_tensor(condition_imgs[i])
+            return_dict[f"condition_type_{i}"] = self.condition_type[i]
+            return_dict[f"position_delta_{i}"] = position_deltas[i]
+            return_dict[f"position_scale_{i}"] = position_scale
+
+        return return_dict
+
 
 
 @torch.no_grad()
