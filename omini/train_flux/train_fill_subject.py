0847824d3d58726459bf137996ca4bb2ed0dde5e
Elizaveta Mironovich <elizavetamironovich@gmail.com>
2026-02-06 20:38:37 +0100

    Fill-in the training script for fill+subject

--- omini/train_flux/train_fill_subject.py
+++ omini/train_flux/train_fill_subject.py
@@ -66,8 +66,8 @@ class FillSubject200KDataset(Subject200KDataset):
             elif c_type == "fill":
                 condition_img= target_image.copy().resize(condition_size).convert("RGB")
                 w, h = condition_img.size
-                x1, x2 = sorted([random.randint(0, w), random.randint(0, w)])
-                y1, y2 = sorted([random.randint(0, h), random.randint(0, h)])
+                x1, x2 = sorted([random.randint(0, w - 1), random.randint(0, w - 1)])
+                y1, y2 = sorted([random.randint(0, h - 1), random.randint(0, h - 1)])
                 mask = Image.new("L", condition_img.size, 0)
                 draw = ImageDraw.Draw(mask)
                 draw.rectangle([x1, y1, x2, y2], fill=255)
@@ -97,9 +97,9 @@ class FillSubject200KDataset(Subject200KDataset):
             ]
 
         return_dict = {
-            "image": self.to_tensor(image),
+            "image": self.to_tensor(target_image),
             "description": description,
-            **({"pil_image": [image, condition_img]} if self.return_pil_image else {}),
+            **({"pil_image": [target_image, condition_imgs]} if self.return_pil_image else {}),
         }
 
         # TODO can this position scale be different for different conditions?
@@ -116,8 +116,111 @@ class FillSubject200KDataset(Subject200KDataset):
 
 @torch.no_grad()
 def test_function(model, save_path, file_name):
-    # TODO: Implement the logic to generate a sample using the model
-    raise NotImplementedError("Sample generation not implemented")
+    condition_size = model.training_config["dataset"]["condition_size"]
+    target_size = model.training_config["dataset"]["target_size"]
+
+
+    condition_type = model.training_config["condition_type"]
+    test_list = []
+
+    condition_list = []
+    # Test case1 (in-distribution test case)
+    prompt = "Resting on the picnic table at a lakeside campsite, it's caught in the golden glow of early morning, with mist rising from the water and tall pines casting long shadows behind the scene."
+    for i, c_type in enumerate(condition_type):
+        if c_type == "subject":
+            image = Image.open("assets/test_in.jpg")
+            image = image.resize(condition_size)
+            # More details about position delta can be found in the documentation.
+            position_delta = np.array([0, -condition_size[0] // 16])
+
+            condition = Condition(image, model.adapter_names[i + 2], position_delta)
+        else:
+            image = Image.open("assets/pexels_table.jpg")
+            image = image.resize(condition_size)
+            x1, x2 = 220, 300
+            y1, y2 = 200, 280
+            mask = Image.new("L", image.size, 0)
+            draw = ImageDraw.Draw(mask)
+            draw.rectangle([x1, y1, x2, y2], fill=255)
+            condition_img = Image.composite(
+                image, Image.new("RGB", image.size, (0, 0, 0)), mask
+            )
+            position_delta = np.array([0, 0])
+            condition = Condition(condition_img, model.adapter_names[i + 2], position_delta)
+        condition_list.append(condition)
+
+    test_list.append((condition_list, prompt))
+
+    condition_list = []
+    prompt = "In a bright room. It is placed on a sofa."
+
+    for i, c_type in enumerate(condition_type):
+        if c_type == "subject":
+            image = Image.open("assets/test_out.jpg")
+            image = image.resize(condition_size)
+            # More details about position delta can be found in the documentation.
+            position_delta = np.array([0, -condition_size[0] // 16])
+
+            condition = Condition(image, model.adapter_names[i + 2], position_delta)
+        else:
+            image = Image.open("assets/room_corner.jpg")
+            image = image.resize(condition_size)
+            x1, x2 = 120, 200
+            y1, y2 = 220, 300
+            mask = Image.new("L", image.size, 0)
+            draw = ImageDraw.Draw(mask)
+            draw.rectangle([x1, y1, x2, y2], fill=255)
+            condition_img = Image.composite(
+                image, Image.new("RGB", image.size, (0, 0, 0)), mask
+            )
+            position_delta = np.array([0, 0])
+            condition = Condition(condition_img, model.adapter_names[i + 2], position_delta)
+        condition_list.append(condition)
+
+    test_list.append((condition_list, prompt))
+
+    condition_list = []
+    prompt = "It is outside with a landscape background."
+
+    for i, c_type in enumerate(condition_type):
+        if c_type == "subject":
+            image = Image.open("assets/Stop.png")
+            image = image.resize(condition_size)
+            # More details about position delta can be found in the documentation.
+            position_delta = np.array([0, -condition_size[0] // 16])
+
+            condition = Condition(image, model.adapter_names[i + 2], position_delta)
+        else:
+            # this image already has a mask
+            image = Image.open("assets/85_0000061.jpg")
+            image = image.resize(condition_size)
+            position_delta = np.array([0, 0])
+            condition = Condition(image, model.adapter_names[i + 2], position_delta)
+        condition_list.append(condition)
+
+    test_list.append((condition_list, prompt))
+
+
+
+    # Generate images
+    os.makedirs(save_path, exist_ok=True)
+    for i, (condition_list, prompt) in enumerate(test_list):
+        generator = torch.Generator(device=model.device)
+        generator.manual_seed(42)
+
+        res = generate(
+            model.flux_pipe,
+            prompt=prompt,
+            conditions=condition_list,
+            height=target_size[1],
+            width=target_size[0],
+            generator=generator,
+            model_config=model.model_config,
+            kv_cache=model.model_config.get("independent_condition", False),
+        )
+        file_path = os.path.join(save_path, f"{file_name}_{condition_type}_{i}.jpg")
+        res.images[0].save(file_path)
+
 
 
 def main():
@@ -126,8 +229,40 @@ def main():
     training_config = config["train"]
     torch.cuda.set_device(int(os.environ.get("LOCAL_RANK", 0)))
 
-    # Initialize custom dataset
-    dataset = CustomDataset()
+    # Initialize raw dataset
+    raw_dataset = load_dataset("Yuanshi/Subjects200K")
+
+    # Define filter function to filter out low-quality images from Subjects200K
+    def filter_func(item):
+        if not item.get("quality_assessment"):
+            return False
+        return all(
+            item["quality_assessment"].get(key, 0) >= 5
+            for key in ["compositeStructure", "objectConsistency", "imageQuality"]
+        )
+
+    # Filter dataset
+    if not os.path.exists("./cache/dataset"):
+        os.makedirs("./cache/dataset")
+    data_valid = raw_dataset["train"].filter(
+        filter_func,
+        num_proc=16,
+        cache_file_name="./cache/dataset/data_valid.arrow",
+    )
+
+    # Initialize the dataset
+    dataset = FillSubject200KDataset(
+        data_valid,
+        condition_size=training_config["dataset"]["condition_size"],
+        target_size=training_config["dataset"]["target_size"],
+        image_size=training_config["dataset"]["image_size"],
+        padding=training_config["dataset"]["padding"],
+        condition_type=training_config["condition_type"],
+        drop_text_prob=training_config["dataset"]["drop_text_prob"],
+        drop_image_prob=training_config["dataset"]["drop_image_prob"],
+    )
+
+    cond_n = len(training_config["condition_type"])
 
     # Initialize model
     trainable_model = OminiModel(
@@ -138,6 +273,8 @@ def main():
         optimizer_config=training_config["optimizer"],
         model_config=config.get("model", {}),
         gradient_checkpointing=training_config.get("gradient_checkpointing", False),
+        adapter_names=[None, None, *["default"] * cond_n],
+        # In this setting, all the conditions are using the same LoRA adapter
     )
 
     train(dataset, trainable_model, config, test_function)
